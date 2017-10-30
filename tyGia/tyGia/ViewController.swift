37f266330a99e73716dbec255e1fed6467f412be
linhth <linhtranhai87@gmail.com>
2017-10-30 09:54:36 +0700

    first

---
+++ tyGia/tyGia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tyGia
+//
+//  Created by linhth on 10/30/17.
+//  Copyright © 2017 AdnetPlus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
